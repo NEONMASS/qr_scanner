35d374ae7589d8de07403eb32d94a182cec0263c
Ananth <147618899+NEONMASS@users.noreply.github.com>
2026-03-01 16:05:48 +0530

    Update AppPrivacyInfo.kt

--- app/src/main/java/com/Neo/permissionauditor/model/AppPrivacyInfo.kt
+++ app/src/main/java/com/Neo/permissionauditor/model/AppPrivacyInfo.kt
@@ -1,15 +1,20 @@
-package com.Neo.permissionauditor.model
+package com.Neo.permissionauditor.model // <-- Safely lowercase
+
+enum class RiskLevel { LOW, MEDIUM, HIGH }
 
 data class AppPrivacyInfo(
     val appName: String,
     val packageName: String,
     val isSystemApp: Boolean,
+    
     val hasCameraAccess: Boolean,
+    val isCameraGranted: Boolean, // NEW: Is it actually enabled?
+    
     val hasLocationAccess: Boolean,
+    val isLocationGranted: Boolean, // NEW: Is it actually enabled?
+    
     val hasMicrophoneAccess: Boolean,
+    val isMicrophoneGranted: Boolean, // NEW: Is it actually enabled?
+    
     val riskLevel: RiskLevel
 )
-
-enum class RiskLevel {
-    HIGH, MEDIUM, LOW
-}
